adf577579d4237c131e7b235bb4829b78d48563d
Leonel Castañeda <leodcastaneda@gmail.com>
2021-05-13 19:17:28 +0000

    update s3 prefix name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,5 @@ module "s3-bucket" {
   source        = "app.terraform.io/LeonelCastaneda-training/s3-bucket/aws"
   version       = "1.15.0"
   
-  bucket_prefix = var.prefix
+  bucket_prefix = "${ lower(var.prefix) }-training"
 }
-
